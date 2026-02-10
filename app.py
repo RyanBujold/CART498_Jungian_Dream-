0345c87c5ca6faa67299194a360c8c74c434838f
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2026-02-10 12:57:42 -0500

    Feat/Fix: got the image to display and added css styling

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")  # Securely load API key
 @app.route("/", methods=["GET", "POST"])
 def index():
     result = None
-    result2 = None
+    img_url = None
     if request.method == "POST":
         prompt = request.form["prompt"]
         try:
@@ -21,7 +21,7 @@ def index():
                 input=[{"role": "developer", "content": "You are a dream interpreter. Your job is to analyze and deconstruct the dreams given through the prompts. Respond to the user by describing what you think their dreams are trying to tell them about their own unconcisous. Use Jungian psychology terms like persona, shadow, archetype. "}, 
                           {"role": "user", "content": prompt}],
                           temperature=1.4,
-                          max_output_tokens=50
+                          max_output_tokens=60
             )
             result = response.output_text
         except Exception as e:
@@ -30,17 +30,24 @@ def index():
         try:
             response2 = openai.images.generate(
                 model="gpt-image-1-mini",  
-                prompt=prompt,
+                prompt=prompt+". Dreamlike, Salvador Dali style",
                 quality="medium"
             )
+
+            img_url = "./static/output.png"
             image_bytes = base64.b64decode(response2.data[0].b64_json)
-            with open("output.png", "wb") as f:
-                f.write(image_bytes)
+            with open(img_url, "wb") as f:
+                 f.write(image_bytes)
+            
+
+            # data_uri = response2.data[0].b64_json
+            # img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
+            # print(img_tag)
 
         except Exception as e:
             result2 = f"Error: {str(e)}"
             
-    return render_template("index.html", result=result)
+    return render_template("index.html", result=result, img_url=img_url)
 
 if __name__ == "__main__":
     app.run(debug=True)  # Run locally for testing
